7448cb947d26265e2d4073ef0e68b52527fdbb80
nikink334 <nikink334@gmail.com>
2024-11-26 09:48:50 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title> EasyLearning </title>
+
+    <link rel="stylesheet" href="style.css">
+
+
+</head>
+
+<body>
+    
+
+    <!-- Navbar -->
+
+	<nav class="navbar">
+    	<div class="navdiv">
+        	<div class="logo"> <a href="#" class="nav-link"> EasyLearning </a> </div>
+        	<ul class="nav-list">
+            	<li><a href="#" class="nav-link">Home</a></li>
+                <li><a href="#" class="nav-link">Course</a></li>
+                <li><a href="#" class="nav-link">Resourse</a></li>
+                <li><a href="#" class="nav-link">Blog</a></li>
+                <li><a href="#" class="nav-link">Alumni</a></li>
+            	<li><a href="adminSide\login.php" class="nav-link">Admin</a></li>
+                <li><a href="tutorSide\login.php" class="nav-link">Instructor</a></li>
+                <li><a href="clientSide\login.php" class="nav-link">User</a></li>
+        	</ul>
+    	</div>
+	</nav>  <br/><br/>
+
+	
+   
+
+
+    <!-- FOOTER -->
+
+    <footer class="footerM">
+    <div>
+        <p class='mainHead'> <b> EasyLearning! </b> </p> 
+        <p class='paraAbout'> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem quia libero fugit modi molestiae magni voluptatum veniam iure repellat qui. Odio hic amet ipsa maiores deserunt inventore eos et saepe!</p>
+    </div>
+
+    <div>
+        <p class='mainHead'> <b> Quick Links </b> </p>
+        <ul class="linkFooter">
+            <li><a href="/" class="menu"> Home </a></li>
+            <li><a href="/Seats" class="menu"> Contact </a></li>
+            <li><a href="adminSide\login.php" class="menu"> Admin </a></li>
+            <li><a href="tutorSide\login.php" class="nav-link">Instructor</a></li>
+            <li><a href="clientSide\login.php" class="menu"> User </a></li>
+        </ul>
+    </div>
+
+    <div id="contactID">
+        <p class='mainHead'> <b> Social Media </b> </p>
+        <ul class="linkFooter">
+            <li> 
+                <a href="/"> <img class="imageSocial" src="images\instagram.png" alt="Insta" /> Instagram </a> 
+            </li>
+            <li> 
+                <a href="/"> <img class="imageSocial" src="images\twitter.png" alt="Twit" /> Twitter </a> 
+            </li>
+            <li> 
+                <a href="/"> <img class="imageSocial" src="images\youtubeF.png" alt="You" /> YouTube</a> 
+            </li>
+        </ul>
+    </div>
+
+    <div>
+        <p class='mainHead'> <b> Contact </b> </p>
+        <ul class="linkFooter">
+            <li> <a href="tel: +91 1122334455"> Contact No: +91 9988776655 </a> </li>
+            <li> <a href="mailto: airadva123@gmail.com"> Mail To: easylearning123@gmail.com </a> </li>
+            <li> <a href=""> Address: Office-213, Magarpatta, Pune-28. </a> </li>
+        </ul>
+    </div>
+        
+    </footer>
+
+    <div class="copyright">
+        </hr>
+        Copyright &copy; EasyLearner.com | All rights are reserved.
+    </div>
+    
+    <script src="js\index.js"></script>
+
+</body>
+
+</html>
